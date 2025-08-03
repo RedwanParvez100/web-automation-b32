80f82f9ea5aa955ba9eb4904fbe08187f674ea76
RedwanParvez100 <106326119+RedwanParvez100@users.noreply.github.com>
2025-08-03 21:44:13 +0600

    Initial commit

---
+++ src/test/java/org/example/OpenBrowser.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OpenBrowser extends DriverSetup{
+
+
+    @Test
+    public void testGooglePageTitle() {
+
+        // Load "https://www.google.com" webpage
+        browser.get("https://www.google.com");
+
+        // Get page title
+        String pageTitle = browser.getTitle();
+        System.out.println(pageTitle);
+
+        // Validate title with text "Google"
+        Assert.assertEquals(pageTitle, "Google");
+
+    }
+
+    @Test
+    public void testGooglePageURL() {
+        // Load "https://www.google.com" webpage
+        browser.get("https://www.google.com");
+
+        // Get page title
+        String pageUrl = browser.getCurrentUrl();
+        System.out.println(pageUrl);
+
+        // Validate title with text "Google"
+        Assert.assertEquals(pageUrl, "https://www.google.com/");
+
+    }
+}
